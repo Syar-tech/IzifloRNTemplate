f70fd464e262e2ae551cbf753565eff2e2e3afd4
Syar-tech <51356918+Syar-tech@users.noreply.github.com>
2021-11-22 16:24:07 +0100

    auto install apk

--- template/scripts/publish.sh
+++ template/scripts/publish.sh
@@ -8,16 +8,22 @@ source ./scripts/apply_config.sh $1 $2
 case $2 in
 
     apk)
+    #
+    #   build apk
+    #
         echo 'Assemble sur Android'
         
         cd android
         ./gradlew incrementVersionCode 
-        ENVFILE=".env.${IZI_ENV}" ./gradlew assembleRelease 
+        ENVFILE=".env.${IZI_ENV}" ./gradlew app:assembleRelease 
         cd ../
         open android/app/build/outputs/apk/release
         ;;
     store)
-        echo 'bundle sur Android'
+    #
+    #   build bundle
+    #
+        echo 'Bundle sur Android'
         
         cd android 
         ./gradlew incrementVersionCode 
@@ -26,11 +32,47 @@ case $2 in
 
         open android/app/build/outputs/bundle/release
         ;;
+    install)
+    #
+    #   INSTALL apk to device
+    #
+        echo 'Assemble sur Android and install'
+        
+        cd android
+        echo 'Get package name ...'
+        APP_ID=`./gradlew -q getAppId | tail -n 1`
+        echo "    Package : ${APP_ID}"
+
+        echo 'Increment build number...'
+        VERSION=`./gradlew -q incrementVersionCode | tail -n 1`
+        echo "    Version : ${VERSION}"
+
+        echo "Build Apk ..."
+        ENVFILE=".env.${IZI_ENV}" ./gradlew app:assembleRelease 
+        cd ../
+        
+        FULLNAME="${APP_ID}.${IZI_ENV}-${VERSION}-release.apk"
+        echo "Apk name : ${FULLNAME}"
+
+        echo "install apk ..."
+        FILE="android/app/build/outputs/apk/release/${FULLNAME}"
+        if test -f "$FILE";then
+            adb install -r $FILE
+        else 
+            echo "Le fichier ${FILE} est introuvable."
+            exit 1
+        fi;
+
+        ;;
     ios)
+    #
+    #   Non géré 
+    #
         echo 'Publication sur iOS'
         ;;
     *)
-        echo 'OS non reconnu, essayez parmis : apk , store, ios'
+        echo 'Commande non reconnu, essayez parmis : apk , store, ios'
+        exit 1
         ;;
 esac
 
